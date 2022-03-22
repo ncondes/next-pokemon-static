7f81d8a99fa9af5394ac6772bf80a474af96e148
nicolas conde <ncondes@unal.edu.co>
2022-03-22 00:38:45 -0500

    Incremental Static Regeneration ISG

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -113,17 +113,29 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
    return {
       paths,
-      fallback: false
+      // fallback: false
+      fallback: "blocking",
    }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
    const { name } = params as { name: string };
+   const pokemon = await getPokemonInfo(name);
+
+   if (!pokemon) {
+      return {
+         redirect: {
+            destination: "/",
+            permanent: false,
+         }
+      }
+   }
 
    return {
       props: {
-         pokemon: await getPokemonInfo(name),
-      }
+         pokemon,
+      },
+      revalidate: 86400,
    }
 };
 
